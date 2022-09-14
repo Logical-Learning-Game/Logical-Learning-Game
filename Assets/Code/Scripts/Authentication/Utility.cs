ea81f8bea91a861a6850ff1d46164d85e0ab95a8
chuckboliver <patpumhak@gmail.com>
2022-09-14 15:59:04 +0700

    :heavy_plus_sign: feat: Authentication Utility class

---
+++ Assets/Code/Scripts/Authentication/Utility.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Authentication
+{
+    public static class Utility
+    {
+        public static string RandomBase64URL(uint length)
+        {
+            var rng = new RNGCryptoServiceProvider();
+            byte[] bytes = new byte[length];
+            rng.GetBytes(bytes);
+
+            return Base64URLEncoderNoPadding(bytes);
+        }
+
+        public static string Base64URLEncoderNoPadding(byte[] buffer)
+        {
+            string base64 = Convert.ToBase64String(buffer);
+
+            // Converts base64 to base64url.
+            base64 = base64.Replace("+", "-");
+            base64 = base64.Replace("/", "_");
+            // Strips padding.
+            base64 = base64.Replace("=", "");
+
+            return base64;
+        }
+
+        public static byte[] SHA256(string inputString)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(inputString);
+            var sha256Managed = new SHA256Managed();
+
+            return sha256Managed.ComputeHash(bytes);
+        }
+
+        public static int GetRandomUnusedPort()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+
+            var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+
+            return port;
+        }
+    }
+    
+}
