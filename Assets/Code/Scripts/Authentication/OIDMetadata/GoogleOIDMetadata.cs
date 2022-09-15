d2c3d3f32dad0699992d167194f4b0543be51ab8
chuckboliver <patpumhak@gmail.com>
2022-09-15 19:44:21 +0700

    :heavy_plus_sign: feat: Add Google OpenID Connect Authentication
    - Add Abstract class of OIDC Authentication
    - Add Google OIDC Authentication class
    - Add Model class of Google OID Metadata for deserialization

---
+++ Assets/Code/Scripts/Authentication/OIDMetadata/GoogleOIDMetadata.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace Authentication.OIDMetadata
+{
+    public class GoogleOIDMetadata
+    {
+
+        [JsonProperty("authorization_endpoint")]
+        public string AuthorizationEndpoint { get; set; }
+
+        [JsonProperty("token_endpoint")]
+        public string TokenEndpoint { get; set; }
+    }
+}
