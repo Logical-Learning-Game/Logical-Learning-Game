7acf21bff7f63409a700002ed10515a9b6d3f919
chuckboliver <patpumhak@gmail.com>
2022-09-15 23:41:21 +0700

    :heavy_plus_sign: feat(authentication): Add google user info retrieving functionality
    - Add user info endpoint to google oid metadata field
    - Add google user info model for deserialization

--- Assets/Code/Scripts/Authentication/OIDMetadata/GoogleOIDMetadata.cs
+++ Assets/Code/Scripts/Authentication/OIDMetadata/GoogleOIDMetadata.cs
@@ -10,5 +10,8 @@ namespace Authentication.OIDMetadata
 
         [JsonProperty("token_endpoint")]
         public string TokenEndpoint { get; set; }
+
+        [JsonProperty("userinfo_endpoint")]
+        public string UserInfoEndpoint { get; set; }
     }
 }
