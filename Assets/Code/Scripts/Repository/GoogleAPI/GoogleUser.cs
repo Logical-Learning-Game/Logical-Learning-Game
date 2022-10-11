3068dcb01812eebf32dc66d9941bf986ed38cf48
chuckboliver <patpumhak@gmail.com>
2022-10-12 00:14:56 +0700

    :sparkles: feat: Add GoogleUser model for deserialization

---
+++ Assets/Code/Scripts/Repository/GoogleAPI/GoogleUser.cs
@@ -0,0 +1,17 @@
+﻿
+using Newtonsoft.Json;
+
+namespace Repository.GoogleAPI
+{
+    public class GoogleUser
+    {
+        [JsonProperty("player_id")]
+        public string PlayerID { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
