20692eb8ca7931f04798b9ac26c69c3347f772a3
Ponlapat Sanguansirikul <ppbasleng@gmail.com>
2022-11-13 19:28:31 +0700

    :poop: feat(levelsystem): try to implement basic movement with rotation
    - add function that return front/left/right/back pos of player
    - add isMoving boolean for fixing movement bugs

--- Assets/Code/Scripts/LevelSystem/LevelManager.cs
+++ Assets/Code/Scripts/LevelSystem/LevelManager.cs
@@ -10,8 +10,8 @@ namespace Unity.Game.Level
     public class LevelManager : MonoBehaviour
     {
         public static LevelManager Instance { get; private set; }
-        public int[] playerPosition = new int[] {0,0};
-        
+        public int[] playerPosition = new int[] { 0, 0 };
+
         // Start is called before the first frame update
         void Awake()
         {
@@ -29,25 +29,99 @@ namespace Unity.Game.Level
         // Update is called once per frame
         void Update()
         {
+            // reset movement
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SetPlayerPosition(0, 0);
+                SetPlayerRotation(1, 0);
+            }
+
+            // movement test
             if (Input.GetKeyDown(KeyCode.W))
             {
-                SetPlayerPosition(3, 3);
+                //StartCoroutine(Player.Instance.MoveBy(1, 0));
+                StartCoroutine(Player.Instance.Move(Player.Instance.FrontPos()));
             }
-            if (Input.GetKeyDown(KeyCode.G))
+            if (Input.GetKeyDown(KeyCode.A))
             {
-                Debug.Log(Player.Instance.posX+" : "+ Player.Instance.posZ);
+                //StartCoroutine(Player.Instance.MoveBy(0, 1));
+                StartCoroutine(Player.Instance.Move(Player.Instance.LeftPos()));
             }
-            if (Input.GetKeyDown(KeyCode.V))
+            if (Input.GetKeyDown(KeyCode.D))
             {
-                StartCoroutine(Player.Instance.MoveBy(1, 0));
+                //StartCoroutine(Player.Instance.MoveBy(0, -1));
+                StartCoroutine(Player.Instance.Move(Player.Instance.RightPos()));
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                //StartCoroutine(Player.Instance.MoveBy(-1, 0));
+                StartCoroutine(Player.Instance.Move(Player.Instance.BackPos()));
+            }
+
+
+            // tile from direction test
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                GetMapTile(Player.Instance.FrontPos());
+                //Debug.Log(Player.Instance.FrontPos()[0] + " : " + Player.Instance.FrontPos()[1]);
+            }
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                GetMapTile(Player.Instance.LeftPos());
+                //Debug.Log(Player.Instance.LeftPos()[0] + " : " + Player.Instance.LeftPos()[1]);
+            }
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                GetMapTile(Player.Instance.RightPos());
+
+                //Debug.Log(Player.Instance.RightPos()[0] + " : " + Player.Instance.RightPos()[1]);
+            }
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                GetMapTile(Player.Instance.BackPos());
+                //Debug.Log(Player.Instance.BackPos()[0] + " : " + Player.Instance.BackPos()[1]);
             }
         }
 
-        void SetPlayerPosition(int x,int z)
+        Tile GetMapTile(int[] pos)
+        {
+            if (pos[0] < 0 || pos[0] >= MapManager.Instance.TileObjects.GetLength(0) || pos[1] < 0 || pos[1] >= MapManager.Instance.TileObjects.GetLength(1))
+            {
+                Debug.Log("Out of range");
+                return null;
+            }
+            else
+            {
+                Debug.Log(MapManager.Instance.TileObjects[pos[0], pos[1]].name);
+                return MapManager.Instance.TileObjects[pos[0], pos[1]].GetComponent<Tile>();
+            }
+        }
+
+        void SetPlayerPosition(int x, int z)
         {
             playerPosition[0] = x;
             playerPosition[1] = z;
             Player.Instance.transform.position = new Vector3(playerPosition[0] * MapConfig.TILE_SCALE, Player.Instance.transform.position.y, playerPosition[1] * MapConfig.TILE_SCALE);
+        }
+
+        void SetPlayerRotation(int x, int z)
+        {
+            if (x == 0 && z == 1)
+            {
+                Player.Instance.transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else if (x == 1 && z == 0)
+            {
+                Player.Instance.transform.rotation = Quaternion.Euler(0, 90, 0);
+            }
+            else if (x == 0 && z == -1)
+            {
+                Player.Instance.transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+            else if (x == -1 && z == 0)
+            {
+                Player.Instance.transform.rotation = Quaternion.Euler(0, 270, 0);
+            }
         }
     }
 }
--- Assets/Code/Scripts/Player/Player.cs
+++ Assets/Code/Scripts/Player/Player.cs
@@ -6,6 +6,28 @@ using GlobalConfig;
 public class Player : MonoBehaviour
 {
     public static Player Instance { get; private set; }
+    [SerializeField] private bool isMoving = false;
+    public int[] FrontPos()
+    {
+        Vector3 pos = transform.forward;
+        return new int[] { posX + (int)pos.x, posZ + (int)pos.z };
+    }
+    public int[] LeftPos()
+    {
+        Vector3 pos = Quaternion.Euler(0, -90, 0) * transform.forward;
+        return new int[] { posX + (int)pos.x, posZ + (int)pos.z };
+    }
+    public int[] RightPos()
+    {
+        Vector3 pos = Quaternion.Euler(0, 90, 0) * transform.forward;
+        return new int[] { posX + (int)pos.x, posZ + (int)pos.z };
+    }
+    public int[] BackPos()
+    {
+        Vector3 pos = Quaternion.Euler(0, 180, 0) * transform.forward;
+        return new int[] { posX + (int)pos.x, posZ + (int)pos.z };
+    }
+
     public int posX
     {
         get
@@ -13,7 +35,6 @@ public class Player : MonoBehaviour
             return (int)(transform.position.x / MapConfig.TILE_SCALE);
         }
     }
-
     public int posY
     {
         get
@@ -21,7 +42,6 @@ public class Player : MonoBehaviour
             return (int)(transform.position.y);
         }
     }
-    
     public int posZ
     {
         get
@@ -41,27 +61,54 @@ public class Player : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    public IEnumerator MoveBy(int x, int z)
-    {
-        Debug.Log("test move by " + x + " : " + z);
-        yield return MoveTo(new Vector3((posX+x) * MapConfig.TILE_SCALE, posY, (posZ+z) * MapConfig.TILE_SCALE));
-        Debug.Log("test move ended");
-    }
 
     public IEnumerator MoveTo(Vector3 destination)
     {
-        while (Vector3.Distance(transform.position, destination) >= 0.0001f)
+        if (isMoving)
+        {
+            yield break;
+        }
+        isMoving = true;
+        //rotate first
+        Debug.Log("rotate distance" + Vector3.Distance(transform.forward, destination) + transform.forward + "" + destination);
+        //while (Vector3.Distance(transform.forward, destination) >= 0.0001f)
+        //{
+        //    //Vector3.RotateTowards(transform.forward, rotateDirection, speed * 4 * Time.deltaTime, 0.0f);
+        //    transform.forward = Vector3.RotateTowards(transform.forward, destination, 4 * Time.deltaTime, 0.0f);
+        //    //transform.forward = Vector3.MoveTowards(transform.forward, destination, PlayerConfig.PLAYER_ROTATE_SPEED * Time.deltaTime);
+        //    yield return null;
+        //}
+        //transform.forward = Vector3Int.RoundToInt(destination);
+
+        //then move
+        while (Vector3.Distance(transform.position, destination) >= 0.001f)
         {
             transform.position = Vector3.MoveTowards(transform.position, destination, PlayerConfig.PLAYER_MOVE_SPEED * Time.deltaTime);
             yield return null;
         }
         transform.position = Vector3Int.RoundToInt(destination);
-        
+        isMoving = false;
     }
+
+    public IEnumerator Move(int[] pos)
+    {
+        Debug.Log("test move " + pos[0] + " : " + pos[1]);
+        yield return MoveTo(new Vector3(pos[0] * MapConfig.TILE_SCALE, posY, pos[1] * MapConfig.TILE_SCALE));
+        Debug.Log("test move ended");
+    }
+
+
 
     // Update is called once per frame
     void Update()
     {
+        DrawDebugRay();
+    }
 
+    void DrawDebugRay()
+    {
+        Debug.DrawRay(transform.position, transform.forward * MapConfig.TILE_SCALE / 2, Color.red);
+        Debug.DrawRay(transform.position, Quaternion.Euler(0, -90, 0) * transform.forward * MapConfig.TILE_SCALE / 2, Color.blue);
+        Debug.DrawRay(transform.position, Quaternion.Euler(0, 90, 0) * transform.forward * MapConfig.TILE_SCALE / 2, Color.blue);
     }
 }
