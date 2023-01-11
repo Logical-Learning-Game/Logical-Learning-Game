771beb990e3e4d6b06a05dd2c9a13eab437a4851
Ponlapat Sanguansirikul <ppbasleng@gmail.com>
2023-01-12 00:00:59 +0700

    :sparkles: feat(camera): implement Map View Manager
    - move DestroyMap() and CreateMap() from Awake() to Start()
    - first use of cinemachine virtual camera
    - add new Mapconfig property (MAP_ROTATE_SPEED)
    - add MapLooker and MapCenter to game scene

---
+++ Assets/Code/Scripts/MapSystem/MapViewManager.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using GlobalConfig;
+
+public class MapViewManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //Virtual Camera Plane
+    [SerializeField] private GameObject MapCenterObj;
+    [SerializeField] private GameObject MapCameraObj;
+    [SerializeField] private bool isMapRotating;
+
+    public static MapViewManager Instance { get; private set; }
+    void Start()
+    {
+
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //test rotate camera
+        if (Input.GetKeyDown(KeyCode.LeftBracket))
+        {
+            StartCoroutine(RotateLeft());
+        }
+        if (Input.GetKeyDown(KeyCode.RightBracket))
+        {
+            StartCoroutine(RotateRight());
+        }
+    }
+
+    public GameObject GetMapCenter(float x, float y)
+    {
+        if (!MapCenterObj)
+        {
+            GameObject MapCenter = new GameObject("MapCenter");
+            MapCenterObj = Instantiate(MapCenter);
+
+        }
+        if (!MapCameraObj)
+        {
+            MapCameraObj = GameObject.Find("MapLooker");
+        }
+
+        MapCameraObj.GetComponent<CinemachineVirtualCamera>().Follow = MapCenterObj.transform;
+        Debug.Log(MapCameraObj);
+        MapCenterObj.transform.position = new Vector3((x - 1) * MapConfig.TILE_SCALE / 2, 0, (y - 1) * MapConfig.TILE_SCALE / 2);
+        return MapCenterObj;
+    }
+
+    private void SetIsMapRotating(bool isMapRotating)
+    {
+        this.isMapRotating = isMapRotating;
+    }
+    IEnumerator RotateLeft()
+    {
+        if (isMapRotating) yield break;
+
+        SetIsMapRotating(true);
+        Vector3 direction = Quaternion.Euler(0, -90, 0) * MapCameraObj.transform.forward;
+        while (Vector3.Distance(MapCameraObj.transform.forward, direction) >= 0.01f)
+        {
+            Vector3 splitRotation = Vector3.RotateTowards(MapCameraObj.transform.forward, direction, MapConfig.MAP_ROTATE_SPEED * Time.deltaTime, 0.0f);
+            MapCameraObj.transform.rotation = Quaternion.LookRotation(splitRotation);
+            yield return null;
+        }
+        MapCameraObj.transform.rotation = Quaternion.LookRotation(direction);
+        SetIsMapRotating(false);
+
+    }
+
+    IEnumerator RotateRight()
+    {
+        if (isMapRotating) yield break;
+
+        SetIsMapRotating(true);
+        Vector3 direction = Quaternion.Euler(0, 90, 0) * MapCameraObj.transform.forward;
+        while (Vector3.Distance(MapCameraObj.transform.forward, direction) >= 0.01f)
+        {
+            Vector3 splitRotation = Vector3.RotateTowards(MapCameraObj.transform.forward, direction, MapConfig.MAP_ROTATE_SPEED * Time.deltaTime, 0.0f);
+            MapCameraObj.transform.rotation = Quaternion.LookRotation(splitRotation);
+            yield return null;
+        }
+        MapCameraObj.transform.rotation = Quaternion.LookRotation(direction);
+        SetIsMapRotating(false);
+    }
+
+}
