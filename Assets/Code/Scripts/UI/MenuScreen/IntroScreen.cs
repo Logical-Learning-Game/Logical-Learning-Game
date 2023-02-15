4564d736d1773b3bb7d4c7585e9c294301b4ea4e
Ponlapat Sanguansirikul <ppbasleng@gmail.com>
2023-02-16 00:16:12 +0700

    :white_check_mark: feat(mainmenu): implement MenuScreen
    - add MenuScreen
    - add GoogleSyncScreen,NewGameScreen,IntroScreen (inherit from MenuScreen)
    - restructure PanelScreen and inherit from MenuScreen
    - add Controller template for all MenuScreens
    - resolve ui toolkits bugs
    - etc. (can't recognized everything)

---
+++ Assets/Code/Scripts/UI/MenuScreen/IntroScreen.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+using System;
+
+namespace Unity.Game.UI
+{
+    public class IntroScreen : MenuScreen
+    {
+        public static event Action NewGameClick;
+        public static event Action ContinueClick;
+        public static event Action QuitGameClick;
+
+        //public VisualElement LevelPanel;
+        //public VisualElement StatPanel;
+        //public VisualElement SettingPanel;
+        //public VisualElement HistoryPanel;
+
+        Button NewGameButton;
+        Button ContinueButton;
+        Button QuitGameButton;
+
+        //String ID
+        const string NewGameButtonName = "NewGameButton";
+        const string ContinueButtonName = "ContinueButton";
+        const string QuitGameButtonName = "QuitGameButton";
+
+        void OnEnable()
+        {
+
+            //GameScreen.OpenPanel += OnOpenPanel;
+        }
+
+        void OnDisable()
+        {
+            //GameScreen.OpenPanel -= OnOpenPanel;
+        }
+
+        protected override void SetVisualElements()
+        {
+            base.SetVisualElements();
+
+            NewGameButton = m_Root.Q<Button>(NewGameButtonName);
+            ContinueButton = m_Root.Q<Button>(ContinueButtonName);
+            QuitGameButton = m_Root.Q<Button>(QuitGameButtonName);
+
+            ShowVisualElement(m_Screen, true);
+        }
+
+        protected override void RegisterButtonCallbacks()
+        {
+            NewGameButton?.RegisterCallback<ClickEvent>(ClickNewGame);
+            ContinueButton?.RegisterCallback<ClickEvent>(ClickContinue);
+            QuitGameButton?.RegisterCallback<ClickEvent>(ClickQuitGame);
+        }
+
+        void ClickNewGame(ClickEvent evt)
+        {
+            Debug.Log("ClickNewGame");
+            NewGameClick?.Invoke();
+        }
+
+        void ClickContinue(ClickEvent evt)
+        {
+            Debug.Log("ClickContinue");
+            ContinueClick?.Invoke();
+        }
+
+        void ClickQuitGame(ClickEvent evt)
+        {
+            Debug.Log("ClickQuitGame");
+            QuitGameClick?.Invoke();
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Code/Scripts/UI/MenuScreen/NewGameScreen.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+using System;
+
+namespace Unity.Game.UI
+{
+    public class NewGameScreen : MenuScreen
+    {
+        public static event Action LocalNewGameClick;
+        public static event Action GoogleNewGameClick;
+        public static event Action BackClick;
+        
+
+        Button LocalNewGameButton;
+        Button GoogleNewGameButton;
+        Button BackButton;
+
+        VisualElement DetectSaveModal;
+        Button CancelNewGameButton;
+        Button ConfirmNewGameButton;
+
+        //String ID
+        const string LocalNewGameButtonName = "LocalNewGameButton";
+        const string GoogleNewGameButtonName = "GoogleNewGameButton";
+        const string BackButtonName = "BackButton";
+
+
+        void OnEnable()
+        {
+
+            //GameScreen.OpenPanel += OnOpenPanel;
+        }
+
+        void OnDisable()
+        {
+            //GameScreen.OpenPanel -= OnOpenPanel;
+        }
+
+        protected override void SetVisualElements()
+        {
+            base.SetVisualElements();
+            
+            LocalNewGameButton = m_Root.Q<Button>(LocalNewGameButtonName);
+            GoogleNewGameButton = m_Root.Q<Button>(GoogleNewGameButtonName);
+            BackButton = m_Root.Q<Button>(BackButtonName);
+
+            DetectSaveModal = m_Root.Q<VisualElement>("DetectSaveModal");
+            CancelNewGameButton = m_Root.Q<Button>("CancelNewGameButton");
+            ConfirmNewGameButton = m_Root.Q<Button>("ConfirmNewGameButton");
+            
+            ShowVisualElement(m_Screen, false);
+        }
+
+        protected override void RegisterButtonCallbacks()
+        {
+            LocalNewGameButton?.RegisterCallback<ClickEvent>(ClickLocalNewGame);
+            GoogleNewGameButton?.RegisterCallback<ClickEvent>(ClickGoogleNewGame);
+            BackButton?.RegisterCallback<ClickEvent>(ClickBack);
+
+            CancelNewGameButton?.RegisterCallback<ClickEvent>(ClickCancelNewGame);
+            ConfirmNewGameButton?.RegisterCallback<ClickEvent>(ClickConfirmNewGame);
+        }
+
+        void ClickLocalNewGame(ClickEvent evt)
+        {
+            // if player have local save, show modal
+            // else, go to local new game
+            if (true)
+            {
+                ShowVisualElement(DetectSaveModal,true);
+            }
+            else
+            {
+                LocalNewGameClick?.Invoke();
+            }
+            
+        }
+
+        void ClickGoogleNewGame(ClickEvent evt)
+        {
+            GoogleNewGameClick?.Invoke();
+        }
+
+        void ClickBack(ClickEvent evt)
+        {
+            BackClick?.Invoke();
+           
+        }
+
+        void ClickCancelNewGame(ClickEvent evt)
+        {
+            ShowVisualElement(DetectSaveModal, false);
+        }
+
+        void ClickConfirmNewGame(ClickEvent evt)
+        {
+            ShowVisualElement(DetectSaveModal, false);
+            LocalNewGameClick?.Invoke();
+        }
+    }
+}
